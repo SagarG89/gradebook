ea33d3d879cfead77a7d63ef70fad270210752af
fatpiggus <79659892+fatpiggus@users.noreply.github.com>
2021-04-02 15:50:18 +0100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,142 @@
+using System.Collections.Generic;
+using System;
+
+namespace GradeBook
+{
+
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+
+      public class NamedObject
+    {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        public Book (string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+        public abstract void AddGrade(double grade);
+        public abstract Statistics GetStatistics()
+    }
+
+    public class InMemoryBook : Book
+    { 
+        public InMemoryBook(string name) : base(name)
+        { 
+            grades = new List<double>();
+            Name = name;
+        }
+        
+        public void AddGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                    default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+    }
+
+
+
+        public override void AddGrade(double grade)
+        {
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            
+ 
+            for(var index = 0; index < grades.Count; index += 1)
+            {
+               result.High = Math.Max(grades[index], result.High);
+               result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+            }
+            
+            result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                result.letter = 'A';
+
+                break; case var d when d >= 80.0:
+                result.letter = 'B';
+                break;
+
+                 case var d when d >= 70.0:
+                result.letter = 'C';
+                break;
+
+                 case var d when d >= 60.0:
+                result.letter = 'D';
+                break;
+
+                default:
+                result.letter = 'F';
+                break;
+
+            }
+        
+            return result;
+            }
+            
+        
+        private List<double> grades;
+
+
+        public const string CATEGORY = "Science";
+          
+        
+}    
